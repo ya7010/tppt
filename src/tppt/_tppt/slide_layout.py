c9277149a106921c0b2347a4ebab3ce0523b525d
yassun7010 <yassun7010@outlook.com>
2025-04-26 13:54:24 +0900

    feat: add TitleSlide class with customizable attributes for title, text, date, footer, and slide number

--- src/tppt/_tppt/slide_layout.py
+++ src/tppt/_tppt/slide_layout.py
@@ -1,3 +1,4 @@
+import datetime
 from abc import ABC
 
 from tppt._pptx.slide import SlideBuilder
@@ -10,6 +11,21 @@ class TttpSlideLayout(ABC):
 
 
 class TitleSlide(TttpSlideLayout):
+    def __init__(
+        self,
+        *,
+        title: str,
+        text: str,
+        date: datetime.date | None = None,
+        footer: str | None = None,
+        slide_number: bool = True,
+    ) -> None:
+        self.title = title
+        self.text = text
+        self.date = date
+        self.footer = footer
+        self.slide_number = slide_number
+
     @classmethod
     def builder(cls) -> "SlideBuilder":
         return SlideBuilder()
