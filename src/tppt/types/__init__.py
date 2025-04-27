f6afc7c38e9287b350b9bdc807d55fb7ced95f03
yassun7010 <yassun7010@outlook.com>
2025-04-28 00:17:28 +0900

    refactor: clean up imports and simplify type aliasing in types module

--- src/tppt/types/__init__.py
+++ src/tppt/types/__init__.py
@@ -1,9 +1,9 @@
 """Types module for tppt."""
 
 import pathlib
-from typing import Literal, TypeAlias
+from typing import TypeAlias
 
-from pptx.enum.shapes import MSO_AUTO_SHAPE_TYPE
+from pptx.enum.shapes import MSO_AUTO_SHAPE_TYPE as _MSO_AUTO_SHAPE_TYPE
 
 from tppt.pptx.shape import RangeProps as _RangeProps
 from tppt.pptx.shape.table import TableCellStyle as TableCellStyle
@@ -30,27 +30,7 @@ from ._length import (
 from ._length import (
     Points as Points,
 )
-from ._length import (
-    to_length as to_length,
-)
-from ._length import (
-    to_point as to_point,
-)
 
 FilePath = str | pathlib.Path
-ShapeType: TypeAlias = MSO_AUTO_SHAPE_TYPE
+ShapeType: TypeAlias = _MSO_AUTO_SHAPE_TYPE
 Range: TypeAlias = _RangeProps
-
-SlideLayoutType: TypeAlias = Literal[
-    "TITLE",
-    "TITLE_AND_CONTENT",
-    "SECTION_HEADER",
-    "TWO_CONTENT",
-    "COMPARISON",
-    "TITLE_ONLY",
-    "BLANK",
-    "CONTENT_WITH_CAPTION",
-    "PICTURE_WITH_CAPTION",
-    "TITLE_AND_VERTICAL_TEXT",
-    "VERTICAL_TITLE_AND_TEXT",
-]
