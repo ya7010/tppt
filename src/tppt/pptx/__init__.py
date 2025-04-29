92dd8f632d18533fe3fd265cf9766450d6102a9b
yassun7010 <yassun7010@outlook.com>
2025-04-29 12:11:42 +0900

    refactor: reorganize imports for better structure and clarity

--- src/tppt/pptx/__init__.py
+++ src/tppt/pptx/__init__.py
@@ -1,1 +1,8 @@
 """pptx wrapper implementation."""
+
+from .presentation import Presentation as Presentation
+from .shape.picture import Picture as Picture
+from .shape.text import Text as Text
+from .slide import Slide as Slide
+from .table import Table as Table
+from .table import TableCellStyle as TableCellStyle
